9548d6e9c6a9a4adaa50d3f38707a0cdc0b2f566
Lukasz Pietrzak <pieeetrzak@gmail.com>
2025-01-24 09:39:11 +0100

    Dodanie możliwosci obliczania obj kuli

--- Program1.c
+++ Program1.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
+#include <math.h>
 
-float cuboid_volume(float a, float b, float h) {
+/*float cuboid_volume(float a, float b, float h) {
 return(a*b*h);
        }
 
+
+}
 int main () {
-    float a, b, h;
+    float a, b, h, ;
 
     printf("Podaj dlugosc prostopadloscianu: ");
     scanf("%f", &a);
 
-    printf("Podaj szerokosc prostopadloscianu: ");
-    scanf("%f", &b);
+  //  printf("Podaj szerokosc prostopadloscianu: ");
+  //  scanf("%f", &b);
 
-    printf("Podaj wysokosc prostopadloscianu: ");
-    scanf("%f", &h);
+  //  printf("Podaj wysokosc prostopadloscianu: ");
+  //  scanf("%f", &h);
 
-    printf("\n Objetosc prostopadloscianu: %f \n", cuboid_volume(a, b, h));
-
+   // printf("\n Objetosc prostopadloscianu: %f \n", cuboid_volume(a, b, h));
 }
+*/
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main() {
+    float R, V;
+
+
+    printf("Podaj promien kuli: ");
+    scanf("%f", &R);
+
+    V = (4.0/3.0) * 3.14 * pow(R, 3);
+
+
+    printf("Objetosc kuli o promieniu %.2f wynosi: %.2f\n", R, V);
+
+    return 0;
+}
+
+
+
+
+
