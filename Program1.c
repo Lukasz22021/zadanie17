a76ad8dc20211f2cb70356e77eecb74932489c08
Lukasz Pietrzak <pieeetrzak@gmail.com>
2025-01-24 10:13:53 +0100

    Ulepszona wersja kodu oraz dodanie menu

--- Program1.c
+++ Program1.c
@@ -1,59 +1,53 @@
 #include <stdio.h>
 #include <math.h>
 
-/*float cuboid_volume(float a, float b, float h) {
-return(a*b*h);
-       }
-
-
+float cuboid_volume(float a, float b, float h) {
+    return (a * b * h);
 }
-int main () {
-    float a, b, h, ;
 
-    printf("Podaj dlugosc prostopadloscianu: ");
-    scanf("%f", &a);
-
-  //  printf("Podaj szerokosc prostopadloscianu: ");
-  //  scanf("%f", &b);
-
-  //  printf("Podaj wysokosc prostopadloscianu: ");
-  //  scanf("%f", &h);
-
-   // printf("\n Objetosc prostopadloscianu: %f \n", cuboid_volume(a, b, h));
-}
-*/
-
-
-
-
-
-float sphere_volume(float r){
-    return ((4.0/3.0) * M_PI * (r*r*r));
+float sphere_volume(float r) {
+    return (4.0 / 3.0) * M_PI * (r * r * r);
 }
 
 int main() {
-    float r;
+    int choice;
 
+    while (1) {
+        printf("\nWybierz bryle, dla ktorej chcesz obliczyc objetosc:\n");
+        printf("1. Prostopadloscian\n");
+        printf("2. Kula\n");
+        printf("0. Zakoncz\n");
+        printf("Twoj wybor: ");
+        scanf("%d", &choice);
 
-    printf("Podaj promien kuli: ");
-    scanf("%f", &r);
+        if (choice == 1) {
+            float a, b, h;
+            printf("Podaj dlugosc prostopadloscianu: ");
+            scanf("%f", &a);
 
+            printf("Podaj szerokosc prostopadloscianu: ");
+            scanf("%f", &b);
 
+            printf("Podaj wysokosc prostopadloscianu: ");
+            scanf("%f", &h);
 
-    printf("Objetosc kuli wynosi: %.2f\n", sphere_volume(r));
+            printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
+        }
+        else if (choice == 2) {
+            float r;
+            printf("Podaj promien kuli: ");
+            scanf("%f", &r);
+
+            printf("\nObjetosc kuli: %f\n", sphere_volume(r));
+        }
+        else if (choice == 0) {
+            printf("Program zostaje zakonczony.\n");
+            break;
+        }
+        else {
+            printf("Nieprawidlowy wybor. Prosze wybrac 1, 2 lub 0.\n");
+        }
+    }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
